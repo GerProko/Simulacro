fff925bccd7a93e79f463ea2342be17509ff5db4
gerproko <101721169+GerProko@users.noreply.github.com>
2022-04-07 10:20:16 -0300

    Initial commit

---
+++ Assets/Scripts/EJ1.cs
@@ -0,0 +1,28 @@
+﻿/*Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ1 : MonoBehaviour
+{
+
+    public int num1;
+    public int num2;
+
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros ingresados son iguales");
+        }
+        else if (num1 != num2)
+        {
+            Debug.Log("Los numeros ingresados son diferentes");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EJ2.cs
@@ -0,0 +1,38 @@
+﻿/*Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.
+ Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+ Luego indicar cuánto dinero sobra o falta.*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ2 : MonoBehaviour
+{
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float presupuesto;
+    float dinerosobrante;
+    float dinerofaltante;
+    void Start()
+    {
+        if ((precio1 + precio2 + precio3) > presupuesto)
+        {
+            dinerofaltante = (precio1 + precio2 + precio3) - presupuesto;
+            Debug.Log("La suma de los productos supera el presupuesto. El dinero faltante es " + dinerofaltante);
+        }
+        else if ((precio1 + precio2 + precio3) < presupuesto)
+        {
+            dinerosobrante = presupuesto - (precio1 + precio2 + precio3);
+            Debug.Log("La suma de los productos no supera el presupuesto. El dinero sobrante es " + dinerosobrante);
+        }
+        else if ((precio1 + precio2 + precio3) == presupuesto)
+        {
+            Debug.Log("El precio y el presupuesto son iguales");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
